eb9b76a6d860d2625c19814bf2a86d4d40af548f
hoshimotoyasunori <hoshimotoyasunori@gmail.com>
2022-04-26 13:48:12 +0900

    createsuperuser

--- api/models.py
+++ api/models.py
@@ -34,7 +34,7 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password=None):
         if not email:
             raise ValueError('email is must')
-        user = self.model(email=self.normalize.email(email))
+        user = self.model(email=self.normalize_email(email))
         user.set_password(password)
         user.save(using=self.db)
         return user
